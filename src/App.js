a42a63f7686a79dc704128ae7fb43e9011ad076a
fmonaco83 <fernandomartinmonaco@gmail.com>
2022-08-23 19:28:39 -0300

    white screen react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
- 
+
 import '../src/App.css'
 import { NavBar } from './components/navbar/NavBar'
+import  {ItemListContainer}  from './components/ItemListContainer'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 
 
 const App = () => {
      return (
           <div>
+
             <NavBar/>
+            <ItemListContainer/> 
+
+             
           
           </div>
      );
